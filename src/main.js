d2397cb6bd7ca069b2b03507966ffe44f0185dae
Funchovy <9600716+funchovy@user.noreply.gitee.com>
2021-09-05 19:01:19 +0800

    Login function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入样式表
+import './assets/css/global.css'
+import axios from 'axios'
+//配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http=axios
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
